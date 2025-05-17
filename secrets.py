1a3b63bf0b1d3f7383fb594ac03c6c4ccd4d7a37
sanju234-san <sanjeevnidhir05@gmail.com>
2025-05-17 21:51:07 +0530

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,7 @@
+import streamlit as st
+apikey = "6cmuCiCMWg6oZgSDMb7edxZ_HIS8aOKlpLjDGVIaLYez"
+url = "https://api.us-south.assistant.watson.cloud.ibm.com/instances/8763f5c8-7b72-453e-bb7c-44bd7e75ed80"
+assistant_id = "4416391a-7896-4764-b0fb-e0b6dcd68214"
+[ibm_tts]
+apikey = "L4HZiCdP0js74Zh257CksORqJ8VF_QPQsk-Y38yWBTtX"
+url = "https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/fe0d7bfd-be8c-4605-9cca-89ecac74502a"
